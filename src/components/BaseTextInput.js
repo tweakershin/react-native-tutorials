fab60cd405e4f14bdf058a6023b3d6c592c705d5
신윤수 <us621011@gmail.com>
2019-09-22 19:15:05 +0900

    BaseTextInput 수정

--- src/components/BaseTextInput.js
+++ src/components/BaseTextInput.js
@@ -8,19 +8,19 @@ export default class BaseTextInput extends Component {
   }
   render() {
     return (
-      <View style={{ padding: 10 }}>
+      <View style={{ padding: 10, width: "80%" }}>
         <TextInput
           style={{
             height: 40,
             borderColor: "gray",
             borderWidth: 1,
-            width: "80%"
+            width: "100%"
           }}
           placeholder="입력해주세요."
           onChangeText={text => this.setState({ text })}
           value={this.state.text}
         />
-        <Text style={{ padding: 10, fontSize: 42 }}>
+        <Text style={{ padding: 10, fontSize: 22, width: "100%" }}>
           입력된 글: {this.state.text}
         </Text>
       </View>
