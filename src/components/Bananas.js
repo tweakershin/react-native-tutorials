3a0bac0579f6fa3dd4f98ffc1589b17e5db0d8b8
신윤수 <us621011@gmail.com>
2019-09-22 15:30:19 +0900

    Banana 예제 진짜

--- src/components/Bananas.js
+++ src/components/Bananas.js
@@ -7,6 +7,7 @@ export default class Bananas extends Component {
       uri:
         "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
     };
+    // Image의 Props 이름은 soruce, 값은 pic객체
     return <Image source={pic} style={{ width: 193, height: 110 }} />;
   }
 }
