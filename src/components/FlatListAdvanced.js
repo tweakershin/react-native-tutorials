f179a5bbec4cbfb05463269cbe5137f2687ddea6
신윤수 <us621011@gmail.com>
2019-09-24 16:06:05 +0900

    FlatList Advanced with TextFile

--- src/components/FlatListAdvanced.js
+++ src/components/FlatListAdvanced.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  TextInput
+} from "react-native";
 
 let addressBook = [
   {
@@ -16,9 +22,37 @@ let addressBook = [
     name: "영희",
     phone: "010-5723-8231",
     key: "3"
+  },
+  {
+    name: "민수",
+    phone: "010-2131-8231",
+    key: "4"
   }
 ];
 export default class FlatListAdvanced extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: ""
+    };
+  }
+
+  _renderItem = ({ item, index }) => {
+    return (
+      <TouchableOpacity onPress={this.props.onPress}>
+        <View style={{ flex: 1, flexDirection: "row", padding: 10 }}>
+          <View style={{ flex: 1 }}>
+            <Text style={{ fontSize: 20 }}>{item.name}</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text style={{ fontSize: 20 }}>{item.phone}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   render() {
     return (
       <View
@@ -28,22 +62,19 @@ export default class FlatListAdvanced extends Component {
           width: "100%"
         }}
       >
+        <TextInput
+          style={{ height: 40, borderWidth: 1 }}
+          placeholder="검색어 입력"
+          onChangeText={text => {
+            this.setState({ searchText: text });
+            console.log(this.state.searchText);
+          }}
+          value={this.state.text}
+        />
+
         <FlatList
           data={addressBook}
-          renderItem={({ item, index }) => {
-            return (
-              <TouchableOpacity onPress={this.props.onPress}>
-                <View style={{ flex: 1, flexDirection: "row", padding: 10 }}>
-                  <View style={{ flex: 1 }}>
-                    <Text style={{ fontSize: 20 }}>{item.name}</Text>
-                  </View>
-                  <View style={{ flex: 1 }}>
-                    <Text style={{ fontSize: 20 }}>{item.phone}</Text>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            );
-          }}
+          renderItem={this._renderItem}
           ItemSeparatorComponent={item => {
             return <View style={{ borderWidth: 1 }}></View>;
           }}
